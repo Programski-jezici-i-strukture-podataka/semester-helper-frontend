1c1ca7fb62c6653d3e5101643aea479c4bd93b5d
Rade Radišić <radisic.rade@gmail.com>
2026-03-31 01:05:31 +0200

    Generate Angular app through CLI and add missing files

--- src/app/pages/upload-page.component.ts
+++ src/app/pages/upload-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UploadService } from '../services/upload.service';
+import { UploadService } from '../services/attendance.service';
 
 @Component({
   selector: 'app-upload-page',
